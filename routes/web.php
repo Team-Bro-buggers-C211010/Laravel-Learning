74000df6c4e1c1aa84b05ec030eb28031abd3436
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-09-15 11:51:57 +0600

    feat: add all crud operations for jobs

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // $jobs = Job::all();
-    // dd($jobs);
     return view('home');
 });
 
@@ -35,7 +33,9 @@ Route::get('/jobs/{id}', function ($id) {
     ]);
 });
 
+// store
 Route::post('/jobs', function () {
+    // validation
     request()->validate([
         'title' => [
             'required',
@@ -51,6 +51,47 @@ Route::post('/jobs', function () {
         'salary' => request('salary'),
         'employer_id' => 200
     ]);
+
+    return redirect('/jobs');
+});
+
+// edit page
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', [
+        'job' => $job
+    ]);
+});
+
+// update
+Route::patch('/jobs/{id}', function ($id) {
+    // validation
+    request()->validate([
+        'title' => [
+            'required',
+            'min:3',
+        ],
+        'salary' => [
+            'required',
+        ]
+    ]);
+
+    // findOrFail will throw an exception if the job is not found
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $id);
+});
+
+// destroy
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+
+    $job->delete();
 
     return redirect('/jobs');
 });
